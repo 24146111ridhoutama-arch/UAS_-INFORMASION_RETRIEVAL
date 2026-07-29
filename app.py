51c6e1d4a76043a146b4ed257fa2f7a439a06ed3
24146111ridhoutama-arch <24146111.ridhoutama@gmail.com>
2026-07-29 21:24:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Information Retrieval", layout="wide")
+st.set_page_config(page_title="UAS Information Retrieval")
 
 st.title("📚 UAS Information Retrieval")
 st.write("Nama : Ridho Utama")
-st.write("Mata Kuliah : Information Retrieval")
+st.write("NIM : 24146111")
 
-uploaded_file = st.file_uploader("Upload File CSV", type=["csv"])
+# Membaca data otomatis dari GitHub
+df = pd.read_csv("data.csv")
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+st.subheader("Data Buku")
+st.dataframe(df)
 
-    st.subheader("Data")
-    st.dataframe(df)
+keyword = st.text_input("Cari Judul atau Kategori")
 
-    keyword = st.text_input("Cari Data")
+if keyword:
+    hasil = df[df.astype(str).apply(
+        lambda x: x.str.contains(keyword, case=False)
+    ).any(axis=1)]
 
-    if keyword:
-        hasil = df[df.astype(str).apply(lambda x: x.str.contains(keyword, case=False)).any(axis=1)]
-
-        st.subheader("Hasil Pencarian")
-        st.dataframe(hasil)
-else:
-    st.info("Silakan upload file CSV.")
+    st.subheader("Hasil Pencarian")
+    st.dataframe(hasil)
